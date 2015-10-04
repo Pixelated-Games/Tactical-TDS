f911c06ca88f27441ed26289dd63c7aeaed511de
QuantumInverted <quantuminverted@gmail.com>
2015-10-04 21:29:57 +0100

    Da Ting
    Refer to ting above

---
+++ MultiplayerPrototype/Assets/Scripts/Player/Cam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+
+    public float height;
+    public float dampening;
+
+    private Transform player;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update() {
+        transform.position = Vector3.Lerp(transform.position, player.position + (Vector3.up * height), Time.smoothDeltaTime * dampening);
+    }
+}
